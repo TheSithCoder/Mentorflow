6e1660dce07cab283c3ba1b3f4ba282347759f93
Colin Sergi <picubed314@gmail.com>
2018-02-24 17:50:32 -0500

    Added Chat to taken question

--- app/client/src/requests/singleRequest.js
+++ app/client/src/requests/singleRequest.js
@@ -4,6 +4,7 @@ import {Route} from 'react-router-dom';
 import {Link} from 'react-router-dom';
 import Request from './request.js';
 import {withCookies,Cookies} from 'react-cookie';
+import Chat from './chat.js';
 var axios = require('axios');
 
 class SingleRequest extends Component{
@@ -44,7 +45,7 @@ class SingleRequest extends Component{
         if(!this.state.request.isTaken){
             return <Request request={this.state.request} callback={this.takeRequest}/>
         }else{
-
+            return <Chat request={this.state.request}/>
         }
     }
 }
