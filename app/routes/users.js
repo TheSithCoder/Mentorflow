43487591f63feedf3ba21cd7095487e664931422
Colin Sergi <picubed314@gmail.com>
2018-02-24 10:38:31 -0500

    asdf

--- app/routes/users.js
+++ app/routes/users.js
@@ -4,9 +4,24 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://dbuser:dbpass@ds247178.mlab.com:47178/mahacks');
 var User = require('../model/users.js');
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    var username = req.body.username;
-    User.findOne()
+router.get('/:username', function(req, res, next) {
+    var testUsername = req.params.username;
+    User.findOne({username: testUsername}, function(err, user){
+        console.log(user);
+    })
+    res.json({
+        'received': 1
+    })
 });
+
+router.post('/', function(req,res,next){
+    var newUser = new User();
+    newUser.username = req.body.username;
+    newUser.save(function(err){
+        if(err)
+            res.send(err);
+        res.json({message:"Added users"});
+    });
+})
 
 module.exports = router;
