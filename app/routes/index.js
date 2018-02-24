15d1a81cfa7d556295f74af3af53c5b304cd01e2
Colin Sergi <picubed314@gmail.com>
2018-02-24 10:54:13 -0500

    Added user api

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,6 +6,6 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.use(users);
+router.use('/users', users);
 
 module.exports = router;
--- app/routes/users.js
+++ app/routes/users.js
@@ -4,13 +4,19 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://dbuser:dbpass@ds247178.mlab.com:47178/mahacks');
 var User = require('../model/users.js');
 /* GET users listing. */
+
 router.get('/:username', function(req, res, next) {
     var testUsername = req.params.username;
-    User.findOne({username: testUsername}, function(err, user){
-        console.log(user);
-    })
-    res.json({
-        'received': 1
+    User.find({username: testUsername}, function(err, user){
+        if(user == null){
+            res.json({
+                free: true
+            });
+        }else{
+            res.json({
+                free: false
+            })
+        }
     })
 });
 
