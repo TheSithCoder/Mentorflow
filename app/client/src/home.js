d32cf3d2ed9b44ecafdc69d6d4e2e5d5857e52a7
Regen <30307062+regenerativep@users.noreply.github.com>
2018-02-24 18:54:02 -0500

    home page

--- app/client/src/home.js
+++ app/client/src/home.js
@@ -6,46 +6,59 @@ import {Link} from 'react-router-dom';
 class Home extends Component{
     render(){
         return (
-            <div style={{"margin":"8px","width":"100%","height":"100%"}}>
-                <table>
+            <div style={{"margin":"8px","width":"100%","height":"100%","background-color":"#ccc"}}>
+                <style>
+                    {".buttonspace { background-color: #ddd; text-align: center; padding: 8px; display: inline-block; width: 95%; height: 384px; }"}
+                    {".buttonspace a { font-size: 150%; }"}
+                    {"tr { height: 400px; }"}
+                    {"p { display: block; }"}
+                </style>
+                <table style={{"width":"100%"}}>
                     <tbody>
                         <tr>
-                            <style>
-                                {"td { height: 8em; background-color: #ccc; }"}
-                            </style>
+                            <td colspan="2">
+                                <div style={{"background-color":"#eee", "padding":"1px 16px", "margin-bottom":"8px", "height":"320px"}}>
+                                <br />
+                                <br />
+                                <br />
+                                <br />
+                                
+                                    <p style={{"font-size":"300%", "text-align":"center"}}>Home Page</p>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
                             <td>
-                                <style>
-                                    {".buttonspace { background-color: #ddd; text-align: center; padding: 8px; display: inline-block; width: 80%; height: 90%; }"}
-                                    {".buttonspace a { font-size: 150%; }"}
-                                    {"td { margin: auto; }"}
-                                </style>
-                                <table>
-                                    <tbody>
-                                        <tr>
-                                            <td colspan="2">
-                                                <div style={{"background-color":"#eee", "padding":"1px 16px", "margin-bottom":"8px"}}>
-                                                    <p style={{"font-size":"300%"}}>Home Page</p>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <div class="buttonspace">
-                                                    <Link to='/signup'>Sign Up</Link>
-                                                </div>
-                                            </td>
-                                            <td>
-                                                <div class="buttonspace">
-                                                    <Link to="/signin">Sign In</Link>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
+                                <div class="buttonspace">
+                                <br />
+                                <br />
+                                <br />
+                                <br />
+                                <br />
+                                <br />
+                                <br />
+                                <br />
+                                <br />
+                                <br />
+                                
+                                    <Link to='/signup'>Sign Up</Link>
+
+                                </div>
                             </td>
                             <td>
-                                <div style={{"background-color":"#ddd"}}>
-                                    <p>hey</p>
+                                <div class="buttonspace">
+                                <br />
+                                <br />
+                                <br />
+                                <br />
+                                <br />
+                                <br />
+                                <br />
+                                <br />
+                                <br />
+                                <br />
+                                
+                                    <Link to="/signin">Sign In</Link>
                                 </div>
                             </td>
                         </tr>
