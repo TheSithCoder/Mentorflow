23f9448288b85145e74813c92fa2568b742ecc9a
Colin Sergi <picubed314@gmail.com>
2018-02-24 11:14:56 -0500

    Added the way to sign in new users

--- app/client/src/signin.js
+++ app/client/src/signin.js
@@ -2,20 +2,47 @@ import React, { Component } from 'react';
 import {Switch} from 'react-router-dom';
 import {Route} from 'react-router-dom';
 import {Link} from 'react-router-dom';
-
+var axios = require('axios');
 class SignIn extends Component{
     constructor(props){
         super(props);
+        this.state = {
+            username: ""
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event){
+        this.setState({username:event.target.value});
+    }
+
+    handleSubmit(event){
+        event.preventDefault();
+        var theUsername = this.state.username;
+        axios.get('/api/users/' + theUsername).then(function(response){
+            if(response.data.free == true){
+                axios.post('/api/users/', {username:theUsername}).then(function(response){
+                    console.log(response);
+                    alert('Success');
+                    return true;
+                })
+            }else{
+                alert('Username already used');
+            }
+        })
     }
 
     render(){
         return(
             <div>
+                <form onSubmit={this.handleSubmit}>
                 <h1>Sign in</h1>
                 <p>Sign in to your user</p><br />
                 <p>Username: </p>
-                <input type="text" value=""></input>
-                <input type="button" value="Enter"></input>
+                <input type="text" value={this.state.username}  onChange={this.handleChange}></input>
+                <input type="submit" value="Enter"></input>
+                </form>
             </div>
         )
     }
