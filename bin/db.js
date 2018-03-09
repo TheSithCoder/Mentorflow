cccf6a97024a78c3a07ffdfa8697eb0167aaa2e4
Colin Sergi <picubed314@gmail.com>
2018-03-09 13:59:34 -0500

    Patch the problem with the database not have properly enclosed URI

--- bin/db.js
+++ bin/db.js
@@ -2,4 +2,4 @@ var mongoose = require('mongoose');
 var dbuser = process.env.dbuser || 'dbuser';
 var dbpass = process.env.dbpass || 'dbpass';
 var dburi = process.env.dburi || 'ds247178.mlab.com:47178/mahacks';
-module.exports = mongoose.connect('mongodb://'+dbuser+':'+dbpass+'@'+dburi);
+module.exports = mongoose.connect('mongodb://'+encodeURIComponent(dbuser)+':'+encodeURIComponent(dbpass)+'@'+dburi);
